1cf2d6db1f775f583465cb242deace47bbbd1837
Hardik B Vanza <hardikvanza15@gmail.com>
2026-01-28 21:43:23 +0530

    Change margin units to 'dvh' in HeroText component
    Updated margin units from 'vh' to 'dvh' for responsive design.

--- src/components/home/HeroText.jsx
+++ src/components/home/HeroText.jsx
@@ -2,10 +2,12 @@ import Video from "./Video";
 
 const HeroText = () => {
   return (
-    <div className="font-[font2] pt-1 mt-[73vh] lg:mt-0  text-center font-bold md:mt-[55vh] sm:mt-[65vh] ">
+    <div className="font-[font2] pt-1 mt-[73dvh] lg:mt-0 md:mt-[55dvh] sm:mt-[65dvh] text-center font-bold">
+      
       <div className="lg:text-[9.5vw] text-[12vw] uppercase lg:leading-[7.9vw] leading-[8.5vw] flex justify-center items-center">
         the Power
       </div>
+
       <div className="lg:text-[9.5vw] text-[12vw] uppercase lg:leading-[9.7vw] leading-[10vw] justify-center flex items-start">
         for
         <div className="h-[7vw] w-[16vw] rounded-full lg:mt-2 mt-1 overflow-hidden">
@@ -13,9 +15,11 @@ const HeroText = () => {
         </div>
         Bright
       </div>
+
       <div className="lg:text-[9.5vw] text-[12vw] uppercase lg:leading-[6.9vw] leading-[10vw] flex justify-center items-center">
         Kingdom
       </div>
+
     </div>
   );
 };
