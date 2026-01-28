4628d043364c0ed03be073a130ac9d41361d37f4
Hardik B Vanza <hardikvanza15@gmail.com>
2026-01-28 22:02:12 +0530

    Add rounded corners to video element

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -2,7 +2,7 @@ const Video = () => {
   return (
     <div className="h-full w-full">
       <video
-        className="h-full w-full object-cover"
+        className="h-full w-full object-cover rounded-full"
         autoPlay
         loop
         muted
