1b37e7765d3419eca16c5b46ec225b259e092bd7
Hardik B Vanza <hardikvanza15@gmail.com>
2026-01-28 21:35:03 +0530

    Refactor Home component layout and structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,21 @@ import Video from '../components/home/Video'
 import HeroText from '../components/home/HeroText'
 import BottomText from '../components/home/BottomText'
 
-
 const Home = () => {
   return (
-    
-    <div className='text-white'>
-        <div className='h-screen w-screen fixed'>
-            <Video/>
-        </div>
-        <div className='relative h-screen w-screen pb-3 overflow-hidden flex flex-col justify-between'>
-            <HeroText/>
-            <BottomText/>
-        </div>
+    <div className="text-white overflow-hidden">
+      
+      {/* Background Video */}
+      <div className="fixed inset-0 h-dvh w-full">
+        <Video />
+      </div>
+
+      {/* Content */}
+      <div className="relative h-dvh w-full flex flex-col justify-between pb-3">
+        <HeroText />
+        <BottomText />
+      </div>
+
     </div>
   )
 }
