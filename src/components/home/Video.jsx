5b0ba1a3415b196f8229d669fe1ddbded9f3f003
Hardik B Vanza <hardikvanza15@gmail.com>
2026-01-28 22:09:19 +0530

    Update Video.jsx

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -2,7 +2,7 @@ const Video = () => {
   return (
     <div className="h-full w-full">
       <video
-        className="h-full w-full object-cover rounded-full"
+        className="h-full w-full object-cover "
         autoPlay
         loop
         muted
